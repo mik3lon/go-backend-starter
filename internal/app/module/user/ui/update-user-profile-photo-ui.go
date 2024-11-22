7800fca551c464b980d5b6192592a22b349a2617
Miguel García <miguelgarciaponce85@gmail.com>
2024-11-22 02:06:08 +0100

    feat() fixing warning

--- internal/app/module/user/ui/update-user-profile-photo-ui.go
+++ internal/app/module/user/ui/update-user-profile-photo-ui.go
@@ -1,8 +1,6 @@
 package user_ui
 
 import (
-	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	user_application "github.com/mik3lon/starter-template/internal/app/module/user/application"
 	http_response "github.com/mik3lon/starter-template/internal/pkg/infrastructure/http/response"
@@ -25,18 +23,21 @@ func NewUpdateUserProfilePhoto(
 }
 
 func (uup *UpdateUserProfilePhoto) HandleUpdateProfilePhoto(g *gin.Context) {
+	// Retrieve email from context
 	email, exists := g.Get("user_email")
 	if !exists {
-		g.JSON(http.StatusBadRequest, gin.H{"error": errors.New("email not exists").Error()})
+		g.JSON(http.StatusBadRequest, gin.H{"error": "email not exists"})
 		return
 	}
 
+	// Retrieve the uploaded file
 	file, err := g.FormFile("profile_image")
 	if err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	// Open the file
 	f, err := file.Open()
 	if err != nil {
 		g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -44,28 +45,30 @@ func (uup *UpdateUserProfilePhoto) HandleUpdateProfilePhoto(g *gin.Context) {
 	}
 	defer f.Close()
 
+	// Read file content
 	content, err := io.ReadAll(f)
 	if err != nil {
 		g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	// Dispatch the command
 	err = uup.cb.Dispatch(g, &user_application.UpdateUserProfilePhotoCommand{
 		Email: email.(string),
 		Image: file2.NewFileInfo(
 			file.Filename,
 			file.Header.Get("Content-Type"),
 			file.Size,
-			content),
+			content,
+		),
 	})
 
-	switch err.(type) {
-	case nil:
-		uup.jw.WriteResponse(g.Writer, "", http.StatusNoContent)
-	default:
-		fmt.Printf("error %v", err)
+	// Handle response based on the result of dispatch
+	if err != nil {
 		g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
-	return
+	// Use 204 No Content for successful execution
+	g.Status(http.StatusNoContent)
 }
